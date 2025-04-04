630a5ad204092f22946f3997588de8bfdc7f69c4
matheusrib08 <matheus.rib0809@gmail.com>
2025-04-04 20:55:50 -0300

    LAB6 PRE FINALIZADO

---
+++ LAB6/index.js
@@ -0,0 +1,31 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+let yuri = {
+    x: 0,
+    y: 100,
+    raio: 30,
+    img: new Image(),
+    desenha: function(){
+        this.img.src = 'yuri.png';
+        ctx.beginPath();
+        ctx.drawImage(this.img, this.x- this.raio, this.y - this.raio, 2*this.raio, 2*this.raio);
+        ctx.closePath();
+    }
+}
+function animacao(){
+    ctx.clearRect(0,0,300,300)
+    yuri.desenha();
+    requestAnimationFrame(animacao)
+}
+animacao();
+document.addEventListener('mousemove',function(evento){
+    let rect = canvas.getBoundingClientRect();
+    let x_mouse = evento.clientX - rect.left;
+    let y_mouse = evento.clientY - rect.top;
+    console.log(x_mouse,y_mouse);
+    yuri.x = x_mouse;
+    yuri.y = y_mouse;
+})
+yuri.x = Math.max(yuri.raio, Math.min(x_mouse, canvas.width - yuri.raio));
+            yuri.y = Math.max(yuri.raio, Math.min(y_mouse, canvas.height - yuri.raio));
